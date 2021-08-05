b394a203449195fadcc4ebc29a74ddd86d535355
sirgallo <nickgallo97@gmail.com>
2021-08-05 01:23:42 -0400

    add automatic healthchecks with docker-compose

--- app/startService.js
+++ app/startService.js
@@ -4,10 +4,12 @@ const Server = require('./src/core/server')
 // routes
 const queryRouter = require('./src/routes/query')
 const trieRouter = require('./src/routes/trie')
+const pollRouter = require('./src/routes/poll')
 
 const routes = [
     { path: '/query', router: queryRouter },
-    { path: '/trie', router: trieRouter } 
+    { path: '/trie', router: trieRouter },
+    { path: '/poll', router: pollRouter }
 ]
 
 const server = new Server(routes)
